a151274933db559d749eee855f3aa91040b45331
LiuQhahah <liuq_1995@163.com>
2017-09-24 16:14:47 +0800

    实现动画效果，74课

--- core/src/cn/edu/njupt/liuqhahah/buckyblaster/MyGdxGame.java
+++ core/src/cn/edu/njupt/liuqhahah/buckyblaster/MyGdxGame.java
@@ -2,12 +2,11 @@ package cn.edu.njupt.liuqhahah.buckyblaster;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
 
@@ -15,48 +14,37 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class MyGdxGame extends ApplicationAdapter {
     private SpriteBatch batch;
-    private BitmapFont font;
+    private TextureAtlas shooterAtlas;
+    private Animation animation;
+    private float timePassed = 0;
 
-    private Texture img;
-    private Sprite sprite;
 
     @Override
     public void create() {
 
         batch = new SpriteBatch();
+        shooterAtlas = new TextureAtlas(Gdx.files.internal("shooter.atlas"));
+        animation = new Animation(1 / 3f, shooterAtlas.getRegions());
 
-        img = new Texture("atm.jpg");
-        sprite = new Sprite(img);
-
-
-        font = new BitmapFont();
-        font.setColor(Color.BLUE);
-
-
-        //can not set font size
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
         batch.begin();
-        //显示string，
-        font.draw(batch, "Hello baby dont say maybe", 500, 600);
-        //显示图片
-        sprite.draw(batch);
+        timePassed += Gdx.graphics.getDeltaTime();
+        batch.draw((TextureRegion) animation.getKeyFrame(timePassed, true), 300, 500);
         batch.end();
-
     }
 
     @Override
     public void dispose() {
         //防止内存泄漏
         batch.dispose();
-        font.dispose();
-        img.dispose();
+        shooterAtlas.dispose();
+
 
     }
 }
