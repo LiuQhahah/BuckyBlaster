bffdd3b855e557df06bb59c4b8d1c20d2bd9af26
LiuQhahah <liuq_1995@163.com>
2017-09-24 16:47:52 +0800

    获取屏幕坐标，75课

--- core/src/cn/edu/njupt/liuqhahah/buckyblaster/MyGdxGame.java
+++ core/src/cn/edu/njupt/liuqhahah/buckyblaster/MyGdxGame.java
@@ -2,30 +2,35 @@ package cn.edu.njupt.liuqhahah.buckyblaster;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
 
 
  */
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
     private SpriteBatch batch;
-    private TextureAtlas shooterAtlas;
-    private Animation animation;
-    private float timePassed = 0;
-
+    private BitmapFont font;
+    private int screenWidth, screenHeight;
+    private String message = "Touch me";
 
     @Override
     public void create() {
 
         batch = new SpriteBatch();
-        shooterAtlas = new TextureAtlas(Gdx.files.internal("shooter.atlas"));
-        animation = new Animation(1 / 3f, shooterAtlas.getRegions());
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
 
+        font = new BitmapFont();
+        font.setColor(Color.GREEN);
+        font.getData().scale(5);
+
+        Gdx.input.setInputProcessor(this);
     }
 
     @Override
@@ -33,9 +38,17 @@ public class MyGdxGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        GlyphLayout layout = new GlyphLayout();
+        layout.setText(font, message);
+        float width = layout.width;
+        float height = layout.height;
+
+        float x = screenWidth / 2 - width / 2;
+        float y = screenHeight / 2 + height / 2;
+
+
         batch.begin();
-        timePassed += Gdx.graphics.getDeltaTime();
-        batch.draw((TextureRegion) animation.getKeyFrame(timePassed, true), 300, 500);
+        font.draw(batch, message, x, y);
         batch.end();
     }
 
@@ -43,8 +56,50 @@ public class MyGdxGame extends ApplicationAdapter {
     public void dispose() {
         //防止内存泄漏
         batch.dispose();
-        shooterAtlas.dispose();
+        font.dispose();
 
+    }
 
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        message = "Touch down at " + screenX + ", " + screenY;
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        message = "Touch up at " + screenX + ", " + screenY;
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        message = "Dragging  at " + screenX + ", " + screenY;
+        return true;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
