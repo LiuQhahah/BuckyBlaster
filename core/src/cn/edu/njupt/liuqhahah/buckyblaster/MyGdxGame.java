fa741ec8ad5601e58f51bf7afbd23638169a65e7
LiuQhahah <liuq_1995@163.com>
2017-09-24 17:25:45 +0800

    在屏幕上拖动图片，76课

--- core/src/cn/edu/njupt/liuqhahah/buckyblaster/MyGdxGame.java
+++ core/src/cn/edu/njupt/liuqhahah/buckyblaster/MyGdxGame.java
@@ -2,35 +2,36 @@ package cn.edu.njupt.liuqhahah.buckyblaster;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
 /**
 
 
  */
-public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
+public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener {
     private SpriteBatch batch;
-    private BitmapFont font;
-    private int screenWidth, screenHeight;
-    private String message = "Touch me";
+    private OrthographicCamera camera;
+    private Texture texture;
+    private Sprite sprite;
+
 
     @Override
     public void create() {
 
         batch = new SpriteBatch();
-        screenWidth = Gdx.graphics.getWidth();
-        screenHeight = Gdx.graphics.getHeight();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        texture = new Texture(Gdx.files.internal("atm.jpg"));
+        sprite = new Sprite(texture);
 
-        font = new BitmapFont();
-        font.setColor(Color.GREEN);
-        font.getData().scale(5);
+        sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
 
-        Gdx.input.setInputProcessor(this);
+        Gdx.input.setInputProcessor(new GestureDetector(this));
     }
 
     @Override
@@ -38,17 +39,10 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
         Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        GlyphLayout layout = new GlyphLayout();
-        layout.setText(font, message);
-        float width = layout.width;
-        float height = layout.height;
-
-        float x = screenWidth / 2 - width / 2;
-        float y = screenHeight / 2 + height / 2;
-
+        batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        font.draw(batch, message, x, y);
+        sprite.draw(batch);
         batch.end();
     }
 
@@ -56,50 +50,54 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
     public void dispose() {
         //防止内存泄漏
         batch.dispose();
-        font.dispose();
-
+        texture.dispose();
     }
 
+
     @Override
-    public boolean keyDown(int keycode) {
+    public boolean touchDown(float x, float y, int pointer, int button) {
         return false;
     }
 
     @Override
-    public boolean keyUp(int keycode) {
+    public boolean tap(float x, float y, int count, int button) {
         return false;
     }
 
     @Override
-    public boolean keyTyped(char character) {
+    public boolean longPress(float x, float y) {
         return false;
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        message = "Touch down at " + screenX + ", " + screenY;
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        camera.translate(-deltaX, deltaY);
+        camera.update();
         return true;
     }
 
     @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        message = "Touch up at " + screenX + ", " + screenY;
-        return true;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        message = "Dragging  at " + screenX + ", " + screenY;
-        return true;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
+    public boolean panStop(float x, float y, int pointer, int button) {
         return false;
     }
 
     @Override
-    public boolean scrolled(int amount) {
+    public boolean zoom(float initialDistance, float distance) {
         return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
     }
 }
